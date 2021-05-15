e566b8dd8f8d3100f30665e716ac64c3053928c9
Han Sheng <hanshengliang@outlook.com>
2021-05-15 12:18:00 +0800

    fix intent

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ while True:
             bboxC = detection.location_data.relative_bounding_box
             ih, iw, ic = img.shape
             bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
-        cv2.rectangle(img, bbox, (255, 0, 255), 2)
-        cv2.putText(img, f'{int(detection.score[0] * 100)}%',
+            cv2.rectangle(img, bbox, (255, 0, 255), 2)
+            cv2.putText(img, f'{int(detection.score[0] * 100)}%',
                     (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN,
                     2, (255, 0, 255), 2)
 
